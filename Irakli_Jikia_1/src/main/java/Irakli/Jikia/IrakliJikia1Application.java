361569310d54e0014a1fc2c3e9f02fcee6b2c889
jiksona <47945234+jiksona@users.noreply.github.com>
2020-06-24 19:44:59 +0400

    Add files via upload

---
+++ Irakli_Jikia_1/src/main/java/Irakli/Jikia/IrakliJikia1Application.java
@@ -0,0 +1,13 @@
+package Irakli.Jikia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrakliJikia1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrakliJikia1Application.class, args);
+    }
+
+}
---
+++ Irakli_Jikia_1/src/test/java/Irakli/Jikia/IrakliJikia1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Irakli.Jikia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrakliJikia1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
